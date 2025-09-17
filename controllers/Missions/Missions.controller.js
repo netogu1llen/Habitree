a16fc95ecf624bf171e6c155a63e6e86923d9c1f
SebastianOso <sebastian.oso03@gmail.com>
2025-09-17 12:28:30 -0600

    fix: Cambiar mayuscula en la ruta del render de getMissions

--- controllers/Missions/Missions.controller.js
+++ controllers/Missions/Missions.controller.js
@@ -3,7 +3,7 @@ const Mission = require('../../models/Missions/Missions.model');
 
 exports.getMissions = async (req, res) => {
     const missions = await Mission.fetchAll();
-    res.render('../views/missions/missions', { title: 'Missions', missions, csrfToken: req.csrfToken()});
+    res.render('../views/Missions/missions', { title: 'Missions', missions, csrfToken: req.csrfToken()});
 };
 
 exports.postMissions = async(req,res,next) => {
