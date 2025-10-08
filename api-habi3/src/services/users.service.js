ffdc31be6774e7485c02e3cc3d948a4fefa795bb
SebastianOso <sebastian.oso03@gmail.com>
2025-10-07 23:38:18 -0600

    fix: stats api

--- api-habi3/src/services/users.service.js
+++ api-habi3/src/services/users.service.js
@@ -45,16 +45,15 @@ const getStatsUser = async (id) => {
         u.name,
         u.email,
         u.coins,
-        t.level AS tree_level,
-        r.league AS ranking_league
+        t.level AS tree_level
      FROM user u
      LEFT JOIN tree t ON u.IDUser = t.IDUser
-     LEFT JOIN ranking r ON t.IDTree = r.IDTree
      WHERE u.IDUser = ?`,
     [id]
   );
   return rows;
 };
+
 
 
 const postSignupUser = async (name, email, gender, dateOfBirth, coins, password) => {
